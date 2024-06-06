4c75d9bb0da5e43e7adb165e6f7bfb7a27ee1d05
maximostrovskiy86 <maxim.ostrovskiy86@gmail.com>
2024-06-06 19:18:01 +0200

    add localStorage for video

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,39 +1,39 @@
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
 
-
 const refs = {
-  gallery: document.querySelector(".gallery"),
+  gallery: document.querySelector('.gallery'),
   modal: document.querySelector('.js-modal'),
   backdrop: document.querySelector('.backdrop'),
   closeModalBtn: document.querySelector('.close-modal-btn'),
-}
+};
 
-const createItemGalleryMarkup = (galleryItems) => {
-  return galleryItems.map(elem => {
-    return `<li class="gallery__item">
-            <a class="gallery__link" href="${elem.original}">
+const createItemGalleryMarkup = galleryItems => {
+  return galleryItems
+    .map(elem => {
+      return `<li class="gallery__item">
+                  <a class="gallery__link" href="${elem.original}">
                 <img class="gallery__image" src="${elem.preview}" alt="${elem.description}" />
             </a>
-        </li>`
-  }).join('');
-}
+        </li>`;
+    })
+    .join('');
+};
 
-const renderGallery =  createItemGalleryMarkup(galleryItems);
+const renderGallery = createItemGalleryMarkup(galleryItems);
 
-refs.gallery.insertAdjacentHTML('beforeend', renderGallery)
+refs.gallery.insertAdjacentHTML('beforeend', renderGallery);
 
-let lightbox = new SimpleLightbox('.gallery a', {
+// let lightbox = new SimpleLightbox('.gallery a', {
+new SimpleLightbox('.gallery a', {
   preloading: true,
   isAnimating: false,
   captionType: 'attr',
   captionsData: 'alt',
   captionPosition: 'top',
   captionDelay: 250,
-
 });
-
 
 // lightbox.on('changed.simplelightbox', function () {
 //   alert("RJCZ GHBDTN")
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,29 @@
+import  Player  from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const onPlay = function(data) {
+  localStorage.setItem("videoplayer-current-time", JSON.stringify(data));
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const saveData = localStorage.getItem('videoplayer-current-time');
+
+const { seconds } = JSON.parse(saveData)
+
+player.setCurrentTime(seconds).then(function(second) {
+  console.log('time', second);
+}).catch(function(error) {
+  switch (error.name) {
+    case 'RangeError':
+      console.log('error.name', error.name);
+      break;
+
+    default:
+      console.log('error.name', error);
+      break;
+  }
+});
